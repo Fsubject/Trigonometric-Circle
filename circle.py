2b6c9f7b1171eb87138ffc258d5f9fd626672ba9
Fsubject <47669927+Fsubject@users.noreply.github.com>
2024-10-18 21:23:19 +0200

    splitted the code

---
+++ circle.py
@@ -0,0 +1,38 @@
+import math
+import pygame
+
+
+def get_points_distance(A, B):
+    A_x, B_x = A[0], B[0]
+    A_y, B_y = A[1], B[1]
+    return math.sqrt(pow((A_x - B_x), 2) + pow((A_y - B_y), 2))
+
+
+def get_cot(sin, cos):
+    return cos / sin
+
+
+class Circle:
+    def __init__(self, width, height, radius):
+        self.center = (width / 2, height / 2.5)
+        self.radius = radius
+        self.angles = []
+
+    def create_angle(self, mouse_pos):
+        point_dist = get_points_distance(mouse_pos, self.center)
+        x = self.center[0] + self.radius * ((mouse_pos[0] - self.center[0]) / point_dist)
+        y = self.center[1] + self.radius * ((mouse_pos[1] - self.center[1]) / point_dist)
+
+        angle_radians = math.atan2(self.center[1] - y, x - self.center[0])
+        angle = round(angle_radians * (180 / math.pi))
+
+        self.angles.append([(x, y), angle + 360 if angle < 0 else angle, [math.sin(angle), math.cos(angle), math.tan(angle), get_cot(math.sin(angle), math.cos(angle))]])
+
+    def get_center(self):
+        return self.center
+
+    def get_radius(self):
+        return self.radius
+
+    def get_angles(self):
+        return self.angles
--- main.py
+++ main.py
@@ -1,88 +1,89 @@
 import pygame
 import math
+from circle import Circle
+
+pygame.init()
+pygame.font.init()
+pygame.display.set_caption("Trigonometric Circle")
 
 WIDTH, HEIGHT = 900, 900
 
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
+RED = (255, 0, 0)
 
 
-def get_points_distance(A, B):
-    A_x, B_x = A[0], B[0]
-    A_y, B_y = A[1], B[1]
-    return math.sqrt(pow((A_x - B_x), 2) + pow((A_y - B_y), 2))
+class Window:
+    def __init__(self):
+        self.window = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
 
+        self.font = pygame.font.SysFont("Arial", 18, bold=True)
 
-def get_cot(sin, cos):
-    return cos / sin
+        self.zero = self.font.render("0", False, BLACK)
+        self.one = self.font.render("1", False, BLACK)
+        self.minus_one = self.font.render("-1", False, BLACK)
 
+        self.circle = Circle(WIDTH, HEIGHT, 300)
+        self.c_center = self.circle.get_center()
+        self.c_radius = self.circle.get_radius()
+        self.c_angles = self.circle.get_angles()
 
-def main():
-    pygame.init()
-    pygame.font.init()
+        self.angle_i = -1
 
-    window = pygame.display.set_mode((WIDTH, HEIGHT))
-    clock = pygame.time.Clock()
-    running = True
+    def draw_circle(self):
+        pygame.draw.circle(self.window, color=BLACK, center=self.c_center, width=2, radius=self.c_radius)
+        self.window.blit(self.zero, (self.c_center[0] + 10, self.c_center[1] + 10))
 
-    pygame.display.set_caption("Trigonometric Circle")
+        pygame.draw.line(self.window, BLACK, (self.c_center[0] - 330, self.c_center[1]), (self.c_center[0] + 330, self.c_center[1]), 2)
+        self.window.blit(self.one, (self.c_center[0] + 330, self.c_center[1] + 10))
+        self.window.blit(self.minus_one, (self.c_center[0] - 330, self.c_center[1] + 10))
 
-    font = pygame.font.SysFont("Arial", 18, bold=True)
+        pygame.draw.line(self.window, BLACK, (self.c_center[0], self.c_center[1] - 340), (self.c_center[0], self.c_center[1] + 340), 2)
+        self.window.blit(self.one, (self.c_center[0] + 10, self.c_center[1] - 330))
+        self.window.blit(self.minus_one, (self.c_center[0] + 10, self.c_center[1] + 330))
 
-    center = (WIDTH / 2, HEIGHT / 2.5)
-    radius = 300
-    user_points = []
-    angle_i = -1
+    def run(self):
+        while self.running:
+            self.clock.tick(60)
+            self.window.fill(WHITE)
 
-    while running:
-        clock.tick(60)
-        window.fill(WHITE)
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE:
+                        self.running = False
+                elif event.type == pygame.MOUSEBUTTONDOWN:
+                    mouse_pos = pygame.mouse.get_pos()
+                    self.circle.create_angle(mouse_pos)
+                    self.angle_i += 1
 
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-            elif event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_ESCAPE:
-                    running = False
-            elif event.type == pygame.MOUSEBUTTONDOWN:
-                mouse_pos = pygame.mouse.get_pos()
-                point_dist = get_points_distance(mouse_pos, center)
-                x = center[0] + radius * ((mouse_pos[0] - center[0]) / point_dist)
-                y = center[1] + radius * ((mouse_pos[1] - center[1]) / point_dist)
+            self.draw_circle()
 
-                angle_radians = math.atan2(center[1] - y, x - center[0])
-                angle = round(angle_radians * (180 / math.pi))
+            for idx, point in enumerate(self.c_angles):
+                pygame.draw.circle(self.window, BLACK if idx != self.angle_i else RED, point[0], 4)
+                pygame.draw.line(self.window, BLACK if idx != self.angle_i else RED, self.c_center, point[0], 3)
 
-                user_points.append([(x, y), angle + 360 if angle < 0 else angle, [math.sin(angle), math.cos(angle), math.tan(angle), get_cot(math.sin(angle), math.cos(angle))]])
-                angle_i += 1
+            angle_txt = self.font.render(f"Actual angle: {self.c_angles[self.angle_i][1] if self.angle_i >= 0 else "N/A"}°", False, BLACK)
+            self.window.blit(angle_txt, (30, 30))
+            if self.angle_i >= 0:
+                sin = self.font.render(f"sin {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][0]}", False, BLACK)
+                cos = self.font.render(f"cos {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][1]}", False, BLACK)
+                tan = self.font.render(f"tan {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][2]}", False, BLACK)
+                cot = self.font.render(f"cot {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][3]}", False, BLACK)
 
-        pygame.draw.circle(window, color=BLACK, center=center, width=2, radius=radius)
-        pygame.draw.line(window, BLACK, (center[0] - 340, center[1]),
-                         (center[0] + 340, center[1]), 2)
-        pygame.draw.line(window, BLACK, (center[0], center[1] - 340),
-                         (center[0], center[1] + 340), 2)
+                self.window.blit(sin, (100, HEIGHT - 200))
+                self.window.blit(cos, (100, HEIGHT - 100))
+                self.window.blit(tan, (WIDTH - 350, HEIGHT - 200))
+                self.window.blit(cot, (WIDTH - 350, HEIGHT - 100))
 
-        for point in user_points:
-            pygame.draw.circle(window, BLACK, point[0], 4)
-            pygame.draw.line(window, BLACK, center, point[0], 3)
+            pygame.display.flip()
 
-        angle_txt = font.render(f"Actual angle: {user_points[angle_i][1] if angle_i >= 0 else "N/A"}°", False, BLACK)
-        window.blit(angle_txt, (30, 30))
-        if angle_i >= 0:
-            sin = font.render(f"sin {user_points[angle_i][1]}° = {user_points[angle_i][2][0]}", False, BLACK)
-            cos = font.render(f"cos {user_points[angle_i][1]}° = {user_points[angle_i][2][1]}", False, BLACK)
-            tan = font.render(f"tan {user_points[angle_i][1]}° = {user_points[angle_i][2][2]}", False, BLACK)
-            cot = font.render(f"cot {user_points[angle_i][1]}° = {user_points[angle_i][2][3]}", False, BLACK)
-
-            window.blit(sin, (100, HEIGHT - 200))
-            window.blit(cos, (100, HEIGHT - 100))
-            window.blit(tan, (WIDTH - 350, HEIGHT - 200))
-            window.blit(cot, (WIDTH - 350, HEIGHT - 100))
-
-        pygame.display.flip()
-
-    pygame.quit()
+        pygame.quit()
 
 
 if __name__ == "__main__":
-    main()
+    window = Window()
+    window.run()
