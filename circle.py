31961474bd8b457c97d3976bd9bba9cdfd963580
Fsubject <47669927+Fsubject@users.noreply.github.com>
2024-11-03 18:20:17 +0100

    can now preview the angle degree

--- circle.py
+++ circle.py
@@ -18,15 +18,30 @@ class Circle:
         self.radius = radius
         self.angles = []
 
-    def create_angle(self, mouse_pos) -> None:
+    def create_angle(self, mouse_pos) -> bool:
+        x, y = self.get_xy(mouse_pos)
+        angle_radian = self.get_radian(x, y)
+        angle_degree = round(math.degrees(angle_radian))
+
+        for angle in self.angles:
+            if angle[1] == angle_degree:
+                return False
+
+        self.angles.append([(x, y), angle_degree,
+                            [math.sin(angle_radian), math.cos(angle_radian), math.tan(angle_radian), get_cot(math.sin(angle_radian), math.cos(angle_radian))]])
+        print(self.angles)
+
+        return True
+
+    def get_xy(self, mouse_pos) -> tuple:
         point_dist = get_points_distance(mouse_pos, self.center)
         x = self.center[0] + self.radius * ((mouse_pos[0] - self.center[0]) / point_dist)
         y = self.center[1] + self.radius * ((mouse_pos[1] - self.center[1]) / point_dist)
 
-        angle_radian = math.atan2(self.center[1] - y, x - self.center[0])
+        return x, y
 
-        self.angles.append([(x, y), round(math.degrees(angle_radian)),
-                            [math.sin(angle_radian), math.cos(angle_radian), math.tan(angle_radian), get_cot(math.sin(angle_radian), math.cos(angle_radian))]])
+    def get_radian(self, x, y):
+        return math.atan2(self.center[1] - y, x - self.center[0])
 
     def get_center(self) -> tuple:
         return self.center
--- main.py
+++ main.py
@@ -11,6 +11,7 @@ WIDTH, HEIGHT = 900, 900
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 RED = (255, 0, 0)
+GREEN = (0, 255, 0)
 
 
 class Window:
@@ -31,6 +32,7 @@ class Window:
         self.c_angles = self.circle.get_angles()
 
         self.angle_i = -1
+        self.is_placing = False
 
     def draw_circle(self) -> None:
         pygame.draw.circle(self.window, color=BLACK, center=self.c_center, width=2, radius=self.c_radius)
@@ -49,6 +51,8 @@ class Window:
             self.clock.tick(60)
             self.window.fill(WHITE)
 
+            mouse_pos = pygame.mouse.get_pos()
+
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.running = False
@@ -56,9 +60,13 @@ class Window:
                     if event.key == pygame.K_ESCAPE:
                         self.running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    mouse_pos = pygame.mouse.get_pos()
-                    self.circle.create_angle(mouse_pos)
-                    self.angle_i += 1
+                    self.is_placing = True
+                elif event.type == pygame.MOUSEBUTTONUP:
+                    if self.is_placing:
+                        if self.circle.create_angle(mouse_pos):
+                            self.angle_i += 1
+
+                        self.is_placing = False
 
             self.draw_circle()
 
@@ -66,8 +74,15 @@ class Window:
                 pygame.draw.circle(self.window, BLACK if idx != self.angle_i else RED, point[0], 4)
                 pygame.draw.line(self.window, BLACK if idx != self.angle_i else RED, self.c_center, point[0], 3)
 
-            angle_txt = self.font.render(f"Actual angle: {self.c_angles[self.angle_i][1] if self.angle_i >= 0 else "N/A"}°", False, BLACK)
+            if self.is_placing:
+                x, y = self.circle.get_xy(mouse_pos)
+                pygame.draw.line(self.window, GREEN, self.c_center, (x, y), 3)
+                angle_txt = self.font.render(f"Actual angle: {round(math.degrees(self.circle.get_radian(x, y)))}°", False, BLACK)
+            else:
+                angle_txt = self.font.render(f"Actual angle: {self.c_angles[self.angle_i][1] if self.angle_i >= 0 else "N/A"}°", False, BLACK)
+
             self.window.blit(angle_txt, (30, 30))
+
             if self.angle_i >= 0:
                 sin = self.font.render(f"sin {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][0]}", False, BLACK)
                 cos = self.font.render(f"cos {self.c_angles[self.angle_i][1]}° = {self.c_angles[self.angle_i][2][1]}", False, BLACK)
